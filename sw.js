b9a22ed8a16473a0dae7a024ffbf476eb2d5d7a2
umamiulcinj <ahaxhibrahimi@gmail.com>
2024-01-10 05:08:37 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,53 +1,70 @@
-const dynamicCacheName = 'umami-dynamic-v1';
+const staticCacheName = 'umami-v1';
+const dynamicCacheName = 'umami-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/manifest.json',
+  '/scripts.js',
+  '/styles.css',
+  '/umami.jpg',
+  '/umamipng.png',
+];
 
-self.addEventListener('install', evt => {
-  // No need to cache static assets during installation
-});
-
-self.addEventListener('activate', evt => {
-    evt.waitUntil(
-      caches.keys().then(keys => {
-        return Promise.all(keys
-          .filter(key => key !== dynamicCacheName)
-          .map(key => caches.delete(key))
-        );
-      })
-      .then(() => self.clients.matchAll())
-      .then(clients => {
-        return Promise.all(
-          clients.map(client => {
-            if (client.url && 'navigate' in client) {
-              return client.navigate(client.url);
-            }
-          })
-        );
-      })
-      .then(() => self.clients.claim())
-    );
-  });
-
-self.addEventListener('fetch', evt => {
-  const request = evt.request;
-
-  // Fetch and update dynamic content
-  evt.respondWith(fetchAndUpdate(request));
-});
-
-function fetchAndUpdate(request) {
-  return fetch(request).then(res => {
-    if (!res || res.status !== 200 || res.type !== 'basic') {
-      return res;
-    }
-
-    const clonedResponse = res.clone();
-
-    // Update dynamic cache with the latest response
-    caches.open(dynamicCacheName).then(cache => {
-      cache.put(request, clonedResponse);
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if(keys.length > size){
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
     });
-
-    return res;
-  }).catch(err => {
-    console.error('Fetch error:', err);
   });
-}
+};
+
+// install event
+self.addEventListener('install', evt => {
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+  //console.log('service worker activated');
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      //console.log(keys);
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// fetch events
+self.addEventListener('fetch', evt => {
+  if(evt.request.url.indexOf('firestore.googleapis.com') === -1){
+    evt.respondWith(
+      caches.match(evt.request).then(cacheRes => {
+        return cacheRes || fetch(evt.request).then(fetchRes => {
+          return caches.open(dynamicCacheName).then(cache => {
+            cache.put(evt.request.url, fetchRes.clone());
+            // check cached items size
+            limitCacheSize(dynamicCacheName, 15);
+            return fetchRes;
+          })
+        });
+      }).catch(() => {
+        if(evt.request.url.indexOf('.html') > -1){
+          return caches.match('/pages/fallback.html');
+        } 
+      })
+    );
+  }
+});

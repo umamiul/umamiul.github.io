e76a91380b37896fa39139b79474bd88efde2b2f
umamiulcinj <ahaxhibrahimi@gmail.com>
2024-01-10 01:11:35 +0100

    sw

---
+++ sw.js
@@ -0,0 +1,42 @@
+// service-worker.js
+
+const CACHE_NAME = 'umami-v1';
+const urlsToCache = [
+  '/index.html',
+  '/scripts.js',
+  '/styles.css',
+  '/umami.jpg',
+  '/manifest.json'
+  // Add other static assets you want to cache
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+
+  // Ensure that the new Service Worker is activated immediately
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});

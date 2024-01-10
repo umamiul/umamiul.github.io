28bece64b9925c182798273b94078ed2ee150403
umamiulcinj <ahaxhibrahimi@gmail.com>
2024-01-10 03:30:40 +0100

    sw

---
+++ sw.js
@@ -0,0 +1,46 @@
+const staticCacheName = 'umami-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  'scripts.js',
+  '/app.js',
+  '/manifest.json',
+  '/umami.jpg',
+  '/umamipng.png',
+];
+
+// install event
+self.addEventListener('install', evt => {
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+  //console.log('service worker activated');
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      //console.log(keys);
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+  //console.log('fetch event', evt);
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});

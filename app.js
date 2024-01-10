9218a2b8c5a08bb4dc265191b46243d5c8bd7cc9
umamiulcinj <ahaxhibrahimi@gmail.com>
2024-01-10 05:36:30 +0100

    k

--- app.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Simulate a delay (you might want to replace this with actual loading logic)
-    setTimeout(() => {
-        document.body.classList.add('loaded');
-    }, 2000); // 2000 milliseconds (adjust as needed)
-});

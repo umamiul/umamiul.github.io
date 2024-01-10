9102a8b6f188718a3879db0027e8c9ba535b346b
umamiulcinj <ahaxhibrahimi@gmail.com>
2024-01-10 04:08:48 +0100

    d

--- sw.js
+++ sw.js
@@ -10,35 +10,52 @@ const assets = [
   './umamipng.png',
 ];
 
-// install event
 self.addEventListener('install', evt => {
-  //console.log('service worker installed');
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
-    })
-  );
-});
-
-self.addEventListener('activate', evt => {
+    evt.waitUntil(
+      caches.open(staticCacheName).then((cache) => {
+        console.log('Caching shell assets');
+        cache.addAll(assets);
+      })
+    );
+  });
+  
+  self.addEventListener('activate', evt => {
     evt.waitUntil(
       caches.keys().then(keys => {
         return Promise.all(keys
-          .filter(key => key !== staticCacheName)
+          .filter(key => key !== staticCacheName && key !== dynamicCacheName)
           .map(key => caches.delete(key))
         );
       })
-      .then(() => self.skipWaiting()) // Add this line
     );
   });
-
-// fetch event
-self.addEventListener('fetch', evt => {
-  //console.log('fetch event', evt);
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
-    })
-  );
-});
+  
+  self.addEventListener('fetch', evt => {
+    evt.respondWith(
+      caches.match(evt.request).then(cacheRes => {
+        return cacheRes || fetchAndUpdate(evt.request);
+      })
+    );
+  });
+  
+  function fetchAndUpdate(request) {
+    return fetch(request).then(res => {
+      // Check if the response is valid
+      if (!res || res.status !== 200 || res.type !== 'basic') {
+        return res;
+      }
+  
+      // Clone the response to avoid consuming it
+      const clonedResponse = res.clone();
+  
+      // Update dynamic cache with the latest response
+      caches.open(dynamicCacheName).then(cache => {
+        cache.put(request, clonedResponse);
+      });
+  
+      return res;
+    }).catch(err => {
+      // Handle fetch errors (e.g., no network connection)
+      console.error('Fetch error:', err);
+    });
+  }

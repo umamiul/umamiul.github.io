0436b375db6fc5240f60f18da7d8e11fe910d4c4
umamiulcinj <ahaxhibrahimi@gmail.com>
2024-01-10 01:29:14 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,45 +1,53 @@
-// sw.js
+// service-worker.js
 
-const CACHE_NAME = 'umami-v4';
+const CACHE_NAME = 'umami-v3';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/scripts.js',
+  '/umami.jpg',
+  // Add other static assets you want to cache
+];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((name) => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
-        })
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then((cache) => cache.addAll(urlsToCache))
+        .then(() => self.skipWaiting())
+    );
+  });
+  
+  self.addEventListener('activate', (event) => {
+    event.waitUntil(
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((name) => {
+            if (name !== CACHE_NAME) {
+              return caches.delete(name);
+            }
+          })
+        );
+      })
+    );
+  
+    return self.clients.claim();
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    if (event.request.url.includes('/index.html')) {
+      event.respondWith(
+        caches.match(event.request, { ignoreSearch: true })
+          .then((response) => {
+            return response || fetch(event.request);
+          })
       );
-    })
-  );
-
-  return self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.url.includes('/index.html')) {
-    event.respondWith(
-      caches.match(event.request, { ignoreSearch: true })
-        .then((response) => {
-          return response || fetch(event.request);
-        })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request)
-        .then((response) => {
-          return response || fetch(event.request);
-        })
-    );
-  }
-});
+    } else {
+      event.respondWith(
+        caches.match(event.request)
+          .then((response) => {
+            return response || fetch(event.request);
+          })
+      );
+    }
+  });

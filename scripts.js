61d537b986ac5046b88b05796f7e634fffefbaba
umamiulcinj <ahaxhibrahimi@gmail.com>
2024-01-10 02:38:59 +0100

    update

--- scripts.js
+++ scripts.js
@@ -45,10 +45,3 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
-
-document.addEventListener('DOMContentLoaded', (event) => {
-  // Simulate a delay (you might want to replace this with actual loading logic)
-  setTimeout(() => {
-    document.body.classList.remove('loading');
-  }, 2000); // 2000 milliseconds (adjust as needed)
-});
